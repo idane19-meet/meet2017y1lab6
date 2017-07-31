a00f169acdd23e6243273a950d756ba60e0ed61d
idane19-meet <idane19@meet.mit.edu>
2017-07-31 18:37:27 +0300

    Stopping here

--- shape1.py
+++ shape1.py
@@ -1,10 +1,9 @@
 import turtle
-up_arrow = 'up'
-left_arrow = 'left'
-down_arrow = 'down'
-right_Arrow = 'right'
-spacebar = 'space'
-
+UP_ARROW = 'Up'
+LEFT_ARROW = 'Left'
+DOWN_ARROW = 'Down'
+RIGHT_ARROW = 'Right'
+SPACEBAR = 'space'
 
 UP = 0
 LEFT = 1
@@ -13,31 +12,46 @@ RIGHT = 3
 
 direction = UP
 
-
-
 def up():
     global direction
     direction = UP
-    print('up')
-
+    print('you pressed up')
+    old_pos = turtle.pos()
+    x,y = old_pos   
+    turtle.goto(x, y+10)
+    print(turtle.pos())
 
 def down():
     global direction
     direction = DOWN
-    print('down')
+    print('you pressed down')
+    old_pos = turtle.pos()
+    x,y = old_pos
+    turtle.goto(x, y-10)
+    print(turtle.pos())
 
 def left():
     global direction
     direction = LEFT
-    print('left')
+    print('you pressed left')
+    old_pos = turtle.pos()
+    x,y = old_pos
+    turtle.goto(x-10, y)
+    print(turtle.pos())
 
 def right():
     global direction
     direction = RIGHT
-    print('right')
+    print('you pressed right')
+    old_pos = turtle.pos()
+    x,y = old_pos
+    turtle.goto(x+10, y)
+    print(turtle.pos())
     
-turtle.onkeypress(up, up_arrow)
-turtle.onkeypress(down, down_arrow)
-turtle.onkeypress(left, left_arrow)
-turtle.onkeypress(right, right_arrow)
+turtle.onkeypress(up, UP_ARROW)
+turtle.onkeypress(down, DOWN_ARROW)
+turtle.onkeypress(left, LEFT_ARROW)
+turtle.onkeypress(right, RIGHT_ARROW)
+turtle.onkeypress(turtle.stamp, SPACEBAR)
 turtle.listen()
+turtle.mainloop()
